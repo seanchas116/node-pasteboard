6d35863f69590d1dd0a6a4e16d6dbfbe42dc7e71
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-14 15:58:18 +0900

    Implement text

--- src/mac.mm
+++ src/mac.mm
@@ -1,11 +1,41 @@
 #include <nan.h>
+#import <Cocoa/Cocoa.h>
 
 static void set(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+    if (info.Length() < 1) {
+        Nan::ThrowTypeError("Wrong number of arguments");
+        return;
+    }
+    if (!info[0]->IsObject()) {
+        Nan::ThrowTypeError("Argument must be Object");
+        return;
+    }
+    auto values = info[0]->ToObject();
+    auto textKey = Nan::New("text").ToLocalChecked();
+
+    NSMutableArray<id<NSPasteboardWriting>> *pasteboardItems = [NSMutableArray array];
+
+    if (values->Has(textKey)) {
+        auto text = values->Get(textKey);
+        if (!text->IsString()) {
+            Nan::ThrowTypeError("Text must be String");
+            return;
+        }
+        v8::String::Utf8Value str(text->ToString());
+        [pasteboardItems addObject: [NSString stringWithUTF8String:*str]];
+    }
+
+    auto pasteboard = [NSPasteboard generalPasteboard];
+    [pasteboard writeObjects:pasteboardItems];
 }
 
 static void hasImage(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 }
 static void hasText(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+    auto pasteboard = [NSPasteboard generalPasteboard];
+    auto types = pasteboard.types;
+    bool contains = [types containsObject:NSPasteboardTypeString];
+    info.GetReturnValue().Set(Nan::New(contains));
 }
 static void hasData(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 }
@@ -13,6 +43,9 @@ static void hasData(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 static void getImage(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 }
 static void getText(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+    auto pasteboard = [NSPasteboard generalPasteboard];
+    auto text = [pasteboard stringForType:NSPasteboardTypeString];
+    info.GetReturnValue().Set(Nan::New([text UTF8String]).ToLocalChecked());
 }
 static void getData(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 }
