f7f00dd962399bd726ebd3a0dfd8b7a254c3ecee
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-14 19:18:32 +0900

    Tweak .d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,16 @@
 interface PasteboardValues {
   image?: string
   text?: string
-  data?: { [type: string]: Buffer | string }
+  data?: { [mimeType: string]: Buffer | string }
 }
 
 export function set (values: PasteboardValues)
 
 export function hasImage (): boolean
 export function hasText (): boolean
-export function hasData (type: string): boolean
+export function hasData (mimeType: string): boolean
 
 export function getImage (): string
 export function getText (): string
-export function getDataBuffer (type: string): Buffer
-export function getDataString (type: string): string
+export function getDataBuffer (mimeType: string): Buffer
+export function getDataString (mimeType: string): string
