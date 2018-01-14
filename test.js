3787b93891e0ded24f2333e3e9c57bcb37359770
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-14 16:17:25 +0900

    Add paste test

--- test.js
+++ test.js
@@ -3,9 +3,14 @@ const clipboardy = require('clipboardy')
 const pasteboard = require('.')
 
 describe('pasteboard', () => {
-  it('copy text', () => {
-    const text = 'Hello, pasteboard'
+  it('sets text', () => {
+    const text = 'Hello, copy'
     pasteboard.set({text})
     assert.equal(clipboardy.readSync(), text)
+  })
+  it('gets text', () => {
+    const text = 'Hello, paste'
+    clipboardy.writeSync(text)
+    assert.equal(pasteboard.getText(), text)
   })
 })
