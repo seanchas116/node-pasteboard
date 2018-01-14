e21926505a54c35c377ea01e4df280d81fb1aa86
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-14 19:11:52 +0900

    Set multiple custom data

--- test.js
+++ test.js
@@ -14,21 +14,25 @@ describe('pasteboard', () => {
     assert.equal(pasteboard.getText(), text)
   })
   it('sets/gets custom data string', () => {
-    const type = 'application/x-foo-bar'
+    const type1 = 'application/x-foo-bar'
+    const type2 = 'application/x-hoge-poyo'
     const data = 'Hello, custom data'
     pasteboard.set({
       text: data,
-      data: { [type]: data }
+      data: { [type1]: data, [type2]: data }
     })
     assert.equal(clipboardy.readSync(), data)
-    assert.equal(pasteboard.getDataString(type), data)
+    assert.equal(pasteboard.getDataString(type1), data)
+    assert.equal(pasteboard.getDataString(type2), data)
   })
   it('sets/gets custom data buffer', () => {
-    const type = 'application/x-foo-bar'
+    const type1 = 'application/x-foo-bar'
+    const type2 = 'application/x-hoge-poyo'
     const data = Buffer.from('Hello, custom data')
     pasteboard.set({
-      data: { [type]: data }
+      data: { [type1]: data, [type2]: data }
     })
-    assert.equal(pasteboard.getDataBuffer(type).toString(), data)
+    assert.equal(pasteboard.getDataBuffer(type1).toString(), data)
+    assert.equal(pasteboard.getDataBuffer(type2).toString(), data)
   })
 })
