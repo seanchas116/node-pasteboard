1b64b1759613f1a577d00c4bea6af90d34b0c1d0
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-16 00:18:30 +0900

    Add image get/set test

--- test.js
+++ test.js
@@ -35,4 +35,34 @@ describe('pasteboard', () => {
     assert.equal(pasteboard.getDataBuffer(type1).toString(), data)
     assert.equal(pasteboard.getDataBuffer(type2).toString(), data)
   })
+  it('sets/gets image', () => {
+    const width = 200
+    const height  = 100
+    const data = new Buffer(width * height * 4)
+
+    let i = 0
+    for (let y = 0; y < height; ++y) {
+      for (let x = 0; x < width; ++x) {
+        if (x < 100) {
+          data[i++] = 255
+          data[i++] = 0
+          data[i++] = 0
+          data[i++] = 255
+        } else {
+          data[i++] = 0
+          data[i++] = 255
+          data[i++] = 0
+          data[i++] = 255
+        }
+      }
+    }
+
+    let image = { width, height, data }
+    pasteboard.set({ image })
+
+    let copiedImage = pasteboard.getImage()
+    assert.equal(copiedImage.width, image.width)
+    assert.equal(copiedImage.height, image.height)
+    assert.deepEqual(copiedImage.data.slice(0, 100), image.data.slice(0, 100))
+  })
 })
