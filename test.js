ff252d1bfa9d2ab9ee728897b09eb022d4fefd66
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-20 15:44:31 +0900

    Test hasText/hasImage/hasData

--- test.js
+++ test.js
@@ -8,21 +8,29 @@ describe('pasteboard', () => {
   it('sets text', () => {
     const text = 'Hello, copy'
     pasteboard.set({text})
+    assert.equal(pasteboard.hasText(), true)
     assert.equal(clipboardy.readSync(), text)
   })
   it('gets text', () => {
     const text = 'Hello, paste'
     clipboardy.writeSync(text)
+    assert.equal(pasteboard.hasText(), true)
     assert.equal(pasteboard.getText(), text)
   })
   it('sets/gets custom data string', () => {
     const type1 = 'application/x-foo-bar'
     const type2 = 'application/x-hoge-poyo'
+    const type3 = 'application/x-not-copied'
     const data = 'Hello, custom data'
     pasteboard.set({
       text: data,
       data: { [type1]: data, [type2]: data }
     })
+    assert.equal(pasteboard.hasText(), true)
+    assert.equal(pasteboard.hasImage(), false)
+    assert.equal(pasteboard.hasData(type1), true)
+    assert.equal(pasteboard.hasData(type2), true)
+    assert.equal(pasteboard.hasData(type3), false)
     assert.equal(clipboardy.readSync(), data)
     assert.equal(pasteboard.getDataString(type1), data)
     assert.equal(pasteboard.getDataString(type2), data)
@@ -30,18 +38,27 @@ describe('pasteboard', () => {
   it('sets/gets custom data buffer', () => {
     const type1 = 'application/x-foo-bar'
     const type2 = 'application/x-hoge-poyo'
+    const type3 = 'application/x-not-copied'
     const data = Buffer.from('Hello, custom data')
     pasteboard.set({
       data: { [type1]: data, [type2]: data }
     })
+    assert.equal(pasteboard.hasText(), false)
+    assert.equal(pasteboard.hasImage(), false)
+    assert.equal(pasteboard.hasData(type1), true)
+    assert.equal(pasteboard.hasData(type2), true)
+    assert.equal(pasteboard.hasData(type3), false)
     assert.equal(pasteboard.getDataBuffer(type1).toString(), data)
     assert.equal(pasteboard.getDataBuffer(type2).toString(), data)
+    assert.equal(pasteboard.getDataBuffer(type3), undefined)
   })
   it('sets/gets image', () => {
     const header = 'data:image/png;base64,'
     const pngData = fs.readFileSync('test.png')
     const dataURL = header + pngData.toString('base64')
     pasteboard.set({image: dataURL})
+    assert.equal(pasteboard.hasText(), false)
+    assert.equal(pasteboard.hasImage(), true)
 
     let copiedImage = pasteboard.getImage()
     assert.equal(copiedImage.slice(0, header.length), header)
