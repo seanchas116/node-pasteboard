1a1197504272ebcd08b270b74f92845f8b2a3c98
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-21 20:03:58 +0900

    Make pure virtual

--- src/pasteboard.hh
+++ src/pasteboard.hh
@@ -12,8 +12,8 @@ public:
     virtual ~PasteboardWriter() {}
     virtual void writeText(const std::string &text) = 0;
     virtual void writeImage(const ImageData &image) = 0;
-    virtual void writeData(const std::string &mimeType, std::string &text);
-    virtual void writeData(const std::string &mimeType, const std::vector<uint8_t> &data);
+    virtual void writeData(const std::string &mimeType, std::string &text) = 0;
+    virtual void writeData(const std::string &mimeType, const std::vector<uint8_t> &data) = 0;
 };
 
 std::unique_ptr<PasteboardWriter> createWriter();
@@ -26,8 +26,8 @@ public:
     virtual bool hasData(const std::string &mimeType) = 0;
     virtual std::string readText() = 0;
     virtual ImageData readImage() = 0;
-    virtual std::string readDataString(const std::string &mimeType);
-    virtual std::vector<uint8_t> readDataBuffer(const std::string &mimeType);
+    virtual std::string readDataString(const std::string &mimeType) = 0;
+    virtual std::vector<uint8_t> readDataBuffer(const std::string &mimeType) = 0;
 };
 
 std::unique_ptr<PasteboardReader> createReader();
