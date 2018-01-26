676157dea9abb81f455bab2579fdc19823e6e56a
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-26 22:13:46 +0900

    Type might not be mime

--- index.d.ts
+++ index.d.ts
@@ -7,16 +7,16 @@ interface PasteboardImage {
 interface PasteboardValues {
   image?: PasteboardImage
   text?: string
-  data?: { [mimeType: string]: Buffer | string }
+  data?: { [type: string]: Buffer | string }
 }
 
 export function set (values: PasteboardValues): void
 
 export function hasImage (): boolean
 export function hasText (): boolean
-export function hasData (mimeType: string): boolean
+export function hasData (type: string): boolean
 
 export function getImage (): PasteboardImage | undefined
 export function getText (): string | undefined
-export function getDataBuffer (mimeType: string): Buffer | undefined
-export function getDataString (mimeType: string): string | undefined
+export function getDataBuffer (type: string): Buffer | undefined
+export function getDataString (type: string): string | undefined
